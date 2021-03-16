48a1bc9418d865bb403aa6e24cc8b85618057633
Igor E <igor@avans.no>
2021-03-16 18:06:04 +0100

    added interfaces

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 export declare function testAdd(a: number, b: number): number;
-export { getServerErrorMessage, processServerError, showErrorMessage } from './helpers/ProcessServerError';
+export { getServerErrorMessage, processServerError, showErrorMessage, } from "./helpers/ProcessServerError";
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ export function testAdd(a: number, b: number) {
   return a + b;
 }
 
-export {getServerErrorMessage,processServerError,showErrorMessage} from './helpers/ProcessServerError'
+export {
+  getServerErrorMessage,
+  processServerError,
+  showErrorMessage,
+} from "./helpers/ProcessServerError";
+
