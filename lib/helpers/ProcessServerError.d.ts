f0dfcd791e4687bb2fd4e9c0ed88764338cbcd07
Igor E <igor@avans.no>
2021-03-16 16:08:29 +0100

    added processservererror

---
+++ lib/helpers/ProcessServerError.d.ts
@@ -0,0 +1,9 @@
+declare global {
+    interface Window {
+        strings: any;
+    }
+}
+export declare function processServerError(error: string | Record<string, any>): void;
+export declare function getServerErrorMessage(error: Record<string, any> | string): string;
+export declare function showErrorMessage(title: string, message: string | any, duration?: number): void;
+//# sourceMappingURL=ProcessServerError.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 export declare function testAdd(a: number, b: number): number;
+export { getServerErrorMessage, processServerError, showErrorMessage } from './helpers/ProcessServerError';
 //# sourceMappingURL=index.d.ts.map
