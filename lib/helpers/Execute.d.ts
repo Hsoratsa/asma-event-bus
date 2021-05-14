d86c3096d8f47f07d56ce6b77950b7a923dab5df
IE <igor@avans.no>
2021-05-14 08:29:18 +0200

    build

---
+++ lib/helpers/Execute.d.ts
@@ -0,0 +1,5 @@
+/**
+ * for graphql requests
+*/
+export declare function execute<T = unknown, V = unknown>(variables: T, operation: string, service_url: string, admin_secret: string, session_variables?: V): Promise<any>;
+//# sourceMappingURL=Execute.d.ts.map
