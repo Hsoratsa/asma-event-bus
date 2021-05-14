5289d00288e467cfb59dd49d21f3e5ef6b7beafa
IE <igor@avans.no>
2021-05-14 09:15:13 +0200

    modied execute

--- lib/helpers/Execute.d.ts
+++ lib/helpers/Execute.d.ts
@@ -1,5 +1,6 @@
 /**
  * for graphql requests
-*/
-export declare function execute<T = unknown, V = unknown>(variables: T, operation: string, service_url: string, admin_secret: string, session_variables?: V): Promise<any>;
+ */
+export declare function executeSRV<R, T = Record<string, string>, V = Record<string, string>>(service_url: string, operation: string, variables?: T, admin_secret?: string, session_variables?: V): Promise<R | undefined>;
+export declare function executeFE<R = unknown, T = unknown, K = unknown>(operation: string, service_url: string, variables?: T, headers?: K): Promise<R | undefined>;
 //# sourceMappingURL=Execute.d.ts.map
--- src/helpers/Execute.ts
+++ src/helpers/Execute.ts
@@ -1,34 +1,48 @@
-/** 
- * for graphql requests 
-*/
+/**
+ * for graphql requests
+ */
 
-import { processServerError } from "./ProcessServerError"
+import { processServerError } from './ProcessServerError'
 
-export async function execute<T=unknown,V=unknown>(
-    variables: T,
-    operation: string,
+export async function executeSRV<R, T = Record<string, string>, V = Record<string, string>>(
     service_url: string,
-    admin_secret: string,
-    session_variables?: V
-) {
+    operation: string,
+    variables?: T,
+    admin_secret?: string,
+    session_variables?: V,
+): Promise<R | undefined> {
+    const headers: Record<string, string> = {}
+    if (admin_secret) {
+        headers['x-hasura-admin-secret'] = admin_secret
+    }
     try {
         const fetchResponse = await fetch(service_url, {
             method: 'POST',
             headers: {
-                'x-hasura-admin-secret': admin_secret,
-                content_encoding: 'gzip',
-                ...session_variables
+                ...headers,
+                'content-encoding': 'gzip',
+                ...session_variables,
             },
             body: JSON.stringify({
                 query: operation,
-                variables: variables
-            })
+                variables: variables,
+            }),
         })
         const data = await fetchResponse.json()
         return data
     } catch (e) {
         processServerError(e)
+        return
         //console.error(e)
     }
     //console.log('DEBUG: ', JSON.stringify(data));
+}
+
+export function executeFE<R = unknown, T = unknown, K = unknown>(
+    operation: string,
+    service_url: string,
+    variables?: T,
+    headers?: K,
+) {
+    return executeSRV<R, T, K>(service_url, operation, variables, undefined, headers)
 }
