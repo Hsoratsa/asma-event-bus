1a90363dd41998df20cf18dbe7690c509177ce7a
IE <igor@avans.no>
2021-10-14 15:47:33 +0200

    ts fixes

--- src/helpers/Config.ts
+++ src/helpers/Config.ts
@@ -2,26 +2,27 @@
 type ObjectType<T> = T extends string ?string :T extends boolean ? boolean :never
 
 export function config<T>(env_var: string, default_value: T): ObjectType<T> {
-    const connector = window.location.host.includes('adopus') ? 'adopus' : 'adcuris'
-
-    const srv_url = getSrvUrl(env_var)
-
+    
+    const srv_url = getDynamicSrvUrl(env_var)
+    
     if(srv_url){
         return srv_url as ObjectType<T>
     }
-
-    if (!window._env_cloud?.[connector]) {
-        console.debug('_env_cloud: ', window._env_cloud, 'connector:', connector)
-        throw new Error(
-            'connector conntext ("adopus", "adcuris") is missing! please check that you generated right env.js file',
-        )
+    
+    const connector =
+    (window.location.host.includes('adopus') && 'adopus') ||
+    (window.location.host.includes('adcuris') && 'adcuris') ||
+    ''
+    
+    if(connector){
+        return ((window._env_cloud?.[connector]?.[env_var] ?? default_value)) as ObjectType<T>
     }
 
-    return (window._env_cloud[connector]?.[env_var] ?? default_value) as ObjectType<T>
+    return window.__ENV?.[env_var] as ObjectType<T>
 }
 
 export function configWeb<T>(env_var: string, default_value: T):ObjectType<T> {
-    const srv_url =getSrvUrl(env_var)
+    const srv_url = getDynamicSrvUrl(env_var)
 
     if(srv_url){
         return srv_url as ObjectType<T>
@@ -30,7 +31,7 @@ export function configWeb<T>(env_var: string, default_value: T):ObjectType<T> {
     return  (window.__ENV?.[env_var] ?? default_value) as ObjectType<T>
 }
 
-function getSrvUrl(env_var:string){
+function getDynamicSrvUrl(env_var:string){
     if (env_var.startsWith('SRV')) {
         const env_name = env_var.replace('SRV_', '').toLowerCase()
 
