45a3faa91b0b8671b24c71aaf846daac3f04312f
Astarosh <Astarosh@avansoma.no>
2021-10-26 12:38:13 +0300

    add toLowercaseAndCapitalizeFirstLetter

--- lib/helpers/CapitalizeFistLetter.d.ts
+++ lib/helpers/CapitalizeFistLetter.d.ts
@@ -1,2 +1,3 @@
 export declare function capitalizeFirstLetter(str: string): string;
+export declare function toLowercaseAndCapitalizeFirstLetter(str: string): string;
 //# sourceMappingURL=CapitalizeFistLetter.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,7 @@
 export { getServerErrorMessage, processServerError, showErrorMessage } from './helpers/ProcessServerError';
 export { CURENCY_NOK, NORVEGIAN_PHONE_NUMBER, ORDINARY, PERSONAL_NUMBER, YEARS } from './helpers/FormatNumberConstants';
 export { COUNTRIES, POSTAL_CODE } from './helpers/NorwegianPostalCodes';
-export { capitalizeFirstLetter } from './helpers/CapitalizeFistLetter';
+export { capitalizeFirstLetter, toLowercaseAndCapitalizeFirstLetter } from './helpers/CapitalizeFistLetter';
 export * from './helpers/IsAdcuris';
 export * from './helpers/UseOutsideClick';
 export * from './helpers/IsNotEmpty';
