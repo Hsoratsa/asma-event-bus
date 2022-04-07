05e5e8a47ceffb8d88dfe41c8fe64dc4534a6a41
IE <igor@avans.no>
2022-04-07 10:12:34 +0200

    v1.5.26

---
+++ lib/helpers/generateSrvAuthBindings.d.ts
@@ -0,0 +1,18 @@
+import { IBaseJwtClaims } from "asma-genql-directory/lib";
+import { AxiosResponse } from "axios";
+import { EnvironmentEnums } from "..";
+export declare function generateSrvAuthBindings(SRV_AUTH: string, DEVELOPMENT: boolean, ENVIRONMENT_TO_OPERATE: EnvironmentEnums): {
+    isJwtValid: () => boolean;
+    signin: (url: string, headers?: Record<string, string> | undefined) => Promise<{
+        token: string;
+    }>;
+    srvAuthGet: <R>(url: string, headers?: Record<string, string> | undefined) => Promise<AxiosResponse<R, any>>;
+    signoutAuth: () => Promise<void>;
+    getJwtTokenAsync: () => Promise<string>;
+    getNewJwtToken: () => Promise<string>;
+    getUserId: () => string;
+    getParsedJwt: <R_1 = IBaseJwtClaims>() => R_1 | undefined;
+    getJwtToken: () => string;
+    accessTokenHasExpired: () => boolean;
+};
+//# sourceMappingURL=generateSrvAuthBindings.d.ts.map
