3226a9eafdfc3780dd8c0050df1ab76f68e1b91d
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-16 19:04:18 +0300

    v0.0.7

--- lib/definitions/directory.types.d.ts
+++ lib/definitions/directory.types.d.ts
@@ -1,5 +1,6 @@
 export interface IDirectoryEventBus {
     select_patients: string[];
     select_patient: string;
+    selected_patients_soknad_ids: string[];
 }
 //# sourceMappingURL=directory.types.d.ts.map
--- src/definitions/directory.types.ts
+++ src/definitions/directory.types.ts
@@ -1,4 +1,7 @@
 export interface IDirectoryEventBus {
     select_patients: string[]
+
     select_patient: string
+
+    selected_patients_soknad_ids: string[]
 }
