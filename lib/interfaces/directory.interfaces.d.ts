4d6f23c22770917f67f61272f98f799513f646c1
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-28 16:12:20 +0300

    asma-types

--- lib/interfaces/directory.interfaces.d.ts
+++ lib/interfaces/directory.interfaces.d.ts
@@ -2,9 +2,4 @@ export interface IPatientBasicData {
     id: string;
     name: string;
 }
-export declare enum ArtifactPresenterTypes {
-    Qnr = "qnr",
-    DocumentProxy = "docp",
-    DocumentOutlook = "doco"
-}
 //# sourceMappingURL=directory.interfaces.d.ts.map
