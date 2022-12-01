89697885b1db766b3b6688a85d851099f1dc6c36
Grini Andrei <Andrei.Grini@carasent.com>
2022-12-01 12:57:13 +0200

    v0.0.23

--- src/interfaces/artifact.interfaces.ts
+++ src/interfaces/artifact.interfaces.ts
@@ -1,8 +1,13 @@
-import type { ArtifactPresenterTypes } from 'asma-types/lib'
+import type { ArtifactPresenterTypes, TemplatesContextTypes } from 'asma-types/lib'
 
 export interface IModeratorArtifactBasicData {
     type: ArtifactPresenterTypes
     id: string
     title: string
     patient_id: string
+}
+
+export interface IQnrViewBasicData {
+    qnr_uuid: string
+    target: TemplatesContextTypes
 }
