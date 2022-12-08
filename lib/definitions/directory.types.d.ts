5b707c491779f76e4f2a525adbd2346fd85f8b02
UtopiaEH <utopiaeh01@gmail.com>
2022-12-08 16:20:44 +0200

    added new event

--- lib/definitions/directory.types.d.ts
+++ lib/definitions/directory.types.d.ts
@@ -4,5 +4,9 @@ export interface IDirectoryEventBus {
     select_patient: string;
     selected_patients_soknad_ids: string[];
     patients_basic_data: IPatientBasicData[];
+    users_credentials: {
+        journal_user_id: string;
+        brukerBrukerNavn: string;
+    };
 }
 //# sourceMappingURL=directory.types.d.ts.map
