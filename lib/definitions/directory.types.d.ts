1ca607097f403490b89b568b476dcedc1b12f855
Ion Ciobanu <Ion.Ciobanu@avanssoma.no>
2022-12-26 21:28:24 +0200

    changes

--- lib/definitions/directory.types.d.ts
+++ lib/definitions/directory.types.d.ts
@@ -1,4 +1,4 @@
-import type { IPatientBasicData } from '../interfaces/directory.interfaces';
+import type { IPatientBasicData, IPatientBasicDataWithActno, IPatientIDs } from '../interfaces/directory.interfaces';
 export interface IDirectoryEventBus {
     select_patients: string[];
     select_patient: string;
@@ -8,5 +8,7 @@ export interface IDirectoryEventBus {
         journal_user_id: string;
         brukerBrukerNavn: string;
     };
+    patients_basic_data_with_actno: IPatientBasicDataWithActno[];
+    selected_patients_ids: IPatientIDs[];
 }
 //# sourceMappingURL=directory.types.d.ts.map
--- lib/interfaces/directory.interfaces.d.ts
+++ lib/interfaces/directory.interfaces.d.ts
@@ -2,4 +2,11 @@ export interface IPatientBasicData {
     id: string;
     name: string;
 }
+export interface IPatientBasicDataWithActno extends IPatientBasicData {
+    actno: string;
+}
+export interface IPatientIDs {
+    id: string;
+    actno: string;
+}
 //# sourceMappingURL=directory.interfaces.d.ts.map
