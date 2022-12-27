84b967d7f6a7189eebd17f987d5b2fef644e68ea
UtopiaEH <utopiaeh01@gmail.com>
2022-12-27 12:47:14 +0200

    added users interface

--- src/interfaces/directory.interfaces.ts
+++ src/interfaces/directory.interfaces.ts
@@ -2,6 +2,7 @@ export interface IPatientBasicData {
     id: string
     name: string
 }
+
 export interface IPatientBasicDataWithActno extends IPatientBasicData {
     actno: string
 }
@@ -9,4 +10,19 @@ export interface IPatientBasicDataWithActno extends IPatientBasicData {
 export interface IPatientIDs {
     id: string
     actno: string
+}
+
+interface UserActivity {
+    id: string,
+    name: string,
+    description: string,
+    corespondence_terminated: boolean,
+    start_date: string,
+}
+
+export interface ISelectedUsers extends IPatientBasicData {
+    national_id: string,
+    mobile_phone: string | null,
+    activities: UserActivity[]
+    recipient_relation_ids: string[]
 }
