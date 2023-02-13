0be63806b28efbbf84129798c5b97e980b316b20
Sviataslau <svyatoslav.korshakevich@carasent.com>
2023-02-13 20:21:20 +0300

    add 2 new IDirectoryEventBus

--- lib/definitions/directory.types.d.ts
+++ lib/definitions/directory.types.d.ts
@@ -2,5 +2,7 @@ import type { IPatientFullFata } from '../interfaces/directory.interfaces';
 export interface IDirectoryEventBus {
     users: IPatientFullFata[];
     selected_users: IPatientFullFata[];
+    request_for_users: {};
+    request_for_selected_users: {};
 }
 //# sourceMappingURL=directory.types.d.ts.map
--- src/definitions/directory.types.ts
+++ src/definitions/directory.types.ts
@@ -3,4 +3,6 @@ import type { IPatientFullFata } from '../interfaces/directory.interfaces'
 export interface IDirectoryEventBus {
     users: IPatientFullFata[]
     selected_users: IPatientFullFata[]
+    request_for_users: {}
+    request_for_selected_users: {}
 }
