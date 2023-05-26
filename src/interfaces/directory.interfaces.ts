ca85f9ac75e2950b634154978fe080b6ab8336fa
igor.efrem <igor.efrem@carasent.com>
2023-05-26 09:47:12 +0200

    added jsDoc comments

--- src/interfaces/directory.interfaces.ts
+++ src/interfaces/directory.interfaces.ts
@@ -1,13 +1,20 @@
 export interface IPatientBasicData {
+    /**
+     * @id is user directory_uuid or users directory_id
+     */
     id: string
     name: string
 }
 export interface IUsersForOrders extends IPatientBasicData {
+    /**
+     * @soknad_id is activity_id or activity
+     */
     soknad_id: string
     directory_uuid: string
 }
 
 export interface IPatientFullData extends IPatientBasicData {
+    
     actno: string
     soknad_ids: string[]
 }
