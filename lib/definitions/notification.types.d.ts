0623df269a7d773e25dfa793ed10a19de503da95
Andrei Grini <Andrei.Grini@avanssoma.no>
2023-06-20 15:10:38 +0000

    Merged PR 3355: Added notification buss - rejectable query id
    Added notification buss - rejectable query id
    
    Related work items: #3786

---
+++ lib/definitions/notification.types.d.ts
@@ -0,0 +1,8 @@
+export interface INotificationEventBus {
+    sms_data: ISmsData[];
+    rejected_query_uuid: string;
+}
+export interface ISmsData {
+    content: string;
+}
+//# sourceMappingURL=notification.types.d.ts.map
