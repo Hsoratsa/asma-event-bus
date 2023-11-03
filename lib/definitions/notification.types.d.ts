9158aeb0a917b3c803a0d101b82e729ba903970b
Bularga Alexandru <bularga.alexandru@carasent.com>
2023-11-03 11:59:23 +0000

    Merged in feat/ASMA-2827-change-sms-widget-behavior-to-work-with-dispatcher-not-props (pull request #26)
    feat: ASMA-2827 Added sent attribute to notification event bus
    
    * feat: Added sent attribute to notification event bus
    
    
    Approved-by: Svyatoslav Korshakevich

--- lib/definitions/notification.types.d.ts
+++ lib/definitions/notification.types.d.ts
@@ -1,6 +1,7 @@
 export interface INotificationEventBus {
     sms_data: ISmsData[];
     rejected_query_uuid: string;
+    sent: boolean;
 }
 export interface ISmsData {
     content: string;
--- src/definitions/notification.types.ts
+++ src/definitions/notification.types.ts
@@ -1,10 +1,9 @@
-
 export interface INotificationEventBus {
     sms_data: ISmsData[]
     rejected_query_uuid: string
+    sent: boolean
 }
 
 export interface ISmsData {
     content: string
 }
-
